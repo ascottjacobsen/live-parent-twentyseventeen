b3e1f6bff107a7e583811831c178a19a7d2b6085
Alex Jacobsen <alexjacobsen@Alexs-MacBook-Pro.local>
2019-05-21 12:40:40 +0100

    rmv redun from functions.php

--- functions.php
+++ functions.php
@@ -332,25 +332,25 @@ function twentyseventeen_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 
-	register_sidebar( array(
-		'name'          => __( 'Footer 2', 'twentyseventeen' ),
-		'id'            => 'sidebar-3',
-		'description'   => __( 'Add widgets here to appear in your footer.', 'twentyseventeen' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+// 	register_sidebar( array(
+// 		'name'          => __( 'Footer 2', 'twentyseventeen' ),
+// 		'id'            => 'sidebar-3',
+// 		'description'   => __( 'Add widgets here to appear in your footer.', 'twentyseventeen' ),
+// 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 		'after_widget'  => '</section>',
+// 		'before_title'  => '<h2 class="widget-title">',
+// 		'after_title'   => '</h2>',
+// 	) );
 
-//add custom widget menu
-	register_sidebar( array(
-    'name'          => __( 'Masthead', 'twentyseventeen' ),
-		'id'            => 'masthead-sidebar',
-    'before_widget' => '<div class = "top-widget">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>',
-  ) );
+// //add custom widget menu
+// 	register_sidebar( array(
+//     'name'          => __( 'Masthead', 'twentyseventeen' ),
+// 		'id'            => 'masthead-sidebar',
+//     'before_widget' => '<div class = "top-widget">',
+//     'after_widget'  => '</div>',
+//     'before_title'  => '<h3>',
+//     'after_title'   => '</h3>',
+//   ) );
 
 }
 add_action( 'widgets_init', 'twentyseventeen_widgets_init' );
@@ -603,28 +603,28 @@ require get_parent_theme_file_path( '/inc/icon-functions.php' );
  * wp_enqueue_style($handle, $src, $deps, $ver, $media)
  */
 
- wp_register_script('handlebars', '/wp-content/themes/twentyseventeen-child/assets/js/serving-js/handlebars-v4.1.2.js');
+//  wp_register_script('handlebars', '/wp-content/themes/twentyseventeen-child/assets/js/serving-js/handlebars-v4.1.2.js');
 
- function load_js_assets() {
+//  function load_js_assets() {
 
-		if( is_page( 'contact-us' ) ) {
-			wp_enqueue_script('contact-js', '/wp-content/themes/twentyseventeen/assets/js/contact.js', array('jquery'), '', true);
-		}
+// 		if( is_page( 'contact-us' ) ) {
+// 			wp_enqueue_script('contact-js', '/wp-content/themes/twentyseventeen/assets/js/contact.js', array('jquery'), '', true);
+// 		}
 		
-		if(is_page('serving-opportunities') ) {
-			wp_enqueue_script('contact-js', '/wp-content/themes/twentyseventeen-child/assets/js/serving-js/serving.js', array('jquery', 'wp-api', 'handlebars'), '', true);
-		}
+// 		if(is_page('serving-opportunities') ) {
+// 			wp_enqueue_script('contact-js', '/wp-content/themes/twentyseventeen-child/assets/js/serving-js/serving.js', array('jquery', 'wp-api', 'handlebars'), '', true);
+// 		}
 	
-}
+// }
 
 
-function enqueue_custom_styles() {
-	wp_enqueue_style( 'twenty-seventeen-child-serving-css', '/wp-content/themes/twentyseventeen-child/assets/css/serving.css', false ); 
-}
+// function enqueue_custom_styles() {
+// 	wp_enqueue_style( 'twenty-seventeen-child-serving-css', '/wp-content/themes/twentyseventeen-child/assets/css/serving.css', false ); 
+// }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_custom_styles' );
+// add_action( 'wp_enqueue_scripts', 'enqueue_custom_styles' );
 
-add_action('wp_enqueue_scripts', 'load_js_assets');
+// add_action('wp_enqueue_scripts', 'load_js_assets');
 
 
 
